11dc6d1108f36e2d0211ef474394f135502c4986
Jivesh kumar <jiveshkumar0709@gmail.com>
2026-09-03 00:54:42 +0530

    Delete models directory

--- models/Task.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-module.exports=mongoose.model("Task",new mongoose.Schema({
- title:{type:String,required:true,trim:true,maxlength:120},
- description:{type:String,trim:true,maxlength:500,default:""},
- priority:{type:String,enum:["Low","Medium","High"],default:"Medium"},
- status:{type:String,enum:["Todo","In Progress","Completed"],default:"Todo"},
- dueDate:{type:Date,default:null},
- user:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true}
-},{timestamps:true}));
